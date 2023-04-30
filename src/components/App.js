9827d13fcb420601480e86aca49cde19f9939c7e
sayanpaul186 <116382826+sayanpaul186@users.noreply.github.com>
2023-04-30 15:36:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <div id="main">
         <nav>
-          Reset
+          <button onclick={() => window.history.replace.state(null,null,'/')}>Reset</button>
         </nav>
         <KeyValueStore />
       </div>
